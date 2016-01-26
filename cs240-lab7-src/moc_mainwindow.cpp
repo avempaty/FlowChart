16319b2789d75ad3b369bf7f87cf207c6da36fda
Aneesh Vempaty <avempaty@purdue.edu>
2016-01-25 20:54:52 -0500

    Uploaded files

---
+++ cs240-lab7-src/moc_mainwindow.cpp
@@ -0,0 +1,267 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[42];
+    char stringdata0[501];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 28), // "backgroundButtonGroupClicked"
+QT_MOC_LITERAL(2, 40, 0), // ""
+QT_MOC_LITERAL(3, 41, 16), // "QAbstractButton*"
+QT_MOC_LITERAL(4, 58, 6), // "button"
+QT_MOC_LITERAL(5, 65, 18), // "buttonGroupClicked"
+QT_MOC_LITERAL(6, 84, 2), // "id"
+QT_MOC_LITERAL(7, 87, 10), // "deleteItem"
+QT_MOC_LITERAL(8, 98, 4), // "Open"
+QT_MOC_LITERAL(9, 103, 19), // "pointerGroupClicked"
+QT_MOC_LITERAL(10, 123, 12), // "bringToFront"
+QT_MOC_LITERAL(11, 136, 10), // "sendToBack"
+QT_MOC_LITERAL(12, 147, 12), // "itemInserted"
+QT_MOC_LITERAL(13, 160, 12), // "DiagramItem*"
+QT_MOC_LITERAL(14, 173, 4), // "item"
+QT_MOC_LITERAL(15, 178, 12), // "textInserted"
+QT_MOC_LITERAL(16, 191, 18), // "QGraphicsTextItem*"
+QT_MOC_LITERAL(17, 210, 18), // "currentFontChanged"
+QT_MOC_LITERAL(18, 229, 4), // "font"
+QT_MOC_LITERAL(19, 234, 15), // "fontSizeChanged"
+QT_MOC_LITERAL(20, 250, 4), // "size"
+QT_MOC_LITERAL(21, 255, 17), // "sceneScaleChanged"
+QT_MOC_LITERAL(22, 273, 5), // "scale"
+QT_MOC_LITERAL(23, 279, 16), // "textColorChanged"
+QT_MOC_LITERAL(24, 296, 16), // "itemColorChanged"
+QT_MOC_LITERAL(25, 313, 16), // "lineColorChanged"
+QT_MOC_LITERAL(26, 330, 19), // "textButtonTriggered"
+QT_MOC_LITERAL(27, 350, 19), // "fillButtonTriggered"
+QT_MOC_LITERAL(28, 370, 19), // "lineButtonTriggered"
+QT_MOC_LITERAL(29, 390, 16), // "handleFontChange"
+QT_MOC_LITERAL(30, 407, 12), // "itemSelected"
+QT_MOC_LITERAL(31, 420, 14), // "QGraphicsItem*"
+QT_MOC_LITERAL(32, 435, 5), // "about"
+QT_MOC_LITERAL(33, 441, 13), // "saveTriggered"
+QT_MOC_LITERAL(34, 455, 6), // "saveAs"
+QT_MOC_LITERAL(35, 462, 4), // "copy"
+QT_MOC_LITERAL(36, 467, 3), // "cut"
+QT_MOC_LITERAL(37, 471, 5), // "paste"
+QT_MOC_LITERAL(38, 477, 4), // "undo"
+QT_MOC_LITERAL(39, 482, 4), // "redo"
+QT_MOC_LITERAL(40, 487, 5), // "group"
+QT_MOC_LITERAL(41, 493, 7) // "ungroup"
+
+    },
+    "MainWindow\0backgroundButtonGroupClicked\0"
+    "\0QAbstractButton*\0button\0buttonGroupClicked\0"
+    "id\0deleteItem\0Open\0pointerGroupClicked\0"
+    "bringToFront\0sendToBack\0itemInserted\0"
+    "DiagramItem*\0item\0textInserted\0"
+    "QGraphicsTextItem*\0currentFontChanged\0"
+    "font\0fontSizeChanged\0size\0sceneScaleChanged\0"
+    "scale\0textColorChanged\0itemColorChanged\0"
+    "lineColorChanged\0textButtonTriggered\0"
+    "fillButtonTriggered\0lineButtonTriggered\0"
+    "handleFontChange\0itemSelected\0"
+    "QGraphicsItem*\0about\0saveTriggered\0"
+    "saveAs\0copy\0cut\0paste\0undo\0redo\0group\0"
+    "ungroup"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      30,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,  164,    2, 0x08 /* Private */,
+       5,    1,  167,    2, 0x08 /* Private */,
+       7,    0,  170,    2, 0x08 /* Private */,
+       8,    0,  171,    2, 0x08 /* Private */,
+       9,    1,  172,    2, 0x08 /* Private */,
+      10,    0,  175,    2, 0x08 /* Private */,
+      11,    0,  176,    2, 0x08 /* Private */,
+      12,    1,  177,    2, 0x08 /* Private */,
+      15,    1,  180,    2, 0x08 /* Private */,
+      17,    1,  183,    2, 0x08 /* Private */,
+      19,    1,  186,    2, 0x08 /* Private */,
+      21,    1,  189,    2, 0x08 /* Private */,
+      23,    0,  192,    2, 0x08 /* Private */,
+      24,    0,  193,    2, 0x08 /* Private */,
+      25,    0,  194,    2, 0x08 /* Private */,
+      26,    0,  195,    2, 0x08 /* Private */,
+      27,    0,  196,    2, 0x08 /* Private */,
+      28,    0,  197,    2, 0x08 /* Private */,
+      29,    0,  198,    2, 0x08 /* Private */,
+      30,    1,  199,    2, 0x08 /* Private */,
+      32,    0,  202,    2, 0x08 /* Private */,
+      33,    0,  203,    2, 0x08 /* Private */,
+      34,    0,  204,    2, 0x08 /* Private */,
+      35,    0,  205,    2, 0x08 /* Private */,
+      36,    0,  206,    2, 0x08 /* Private */,
+      37,    0,  207,    2, 0x08 /* Private */,
+      38,    0,  208,    2, 0x08 /* Private */,
+      39,    0,  209,    2, 0x08 /* Private */,
+      40,    0,  210,    2, 0x08 /* Private */,
+      41,    0,  211,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 13,   14,
+    QMetaType::Void, 0x80000000 | 16,   14,
+    QMetaType::Void, QMetaType::QFont,   18,
+    QMetaType::Void, QMetaType::QString,   20,
+    QMetaType::Void, QMetaType::QString,   22,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 31,   14,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->backgroundButtonGroupClicked((*reinterpret_cast< QAbstractButton*(*)>(_a[1]))); break;
+        case 1: _t->buttonGroupClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->deleteItem(); break;
+        case 3: _t->Open(); break;
+        case 4: _t->pointerGroupClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->bringToFront(); break;
+        case 6: _t->sendToBack(); break;
+        case 7: _t->itemInserted((*reinterpret_cast< DiagramItem*(*)>(_a[1]))); break;
+        case 8: _t->textInserted((*reinterpret_cast< QGraphicsTextItem*(*)>(_a[1]))); break;
+        case 9: _t->currentFontChanged((*reinterpret_cast< const QFont(*)>(_a[1]))); break;
+        case 10: _t->fontSizeChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 11: _t->sceneScaleChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 12: _t->textColorChanged(); break;
+        case 13: _t->itemColorChanged(); break;
+        case 14: _t->lineColorChanged(); break;
+        case 15: _t->textButtonTriggered(); break;
+        case 16: _t->fillButtonTriggered(); break;
+        case 17: _t->lineButtonTriggered(); break;
+        case 18: _t->handleFontChange(); break;
+        case 19: _t->itemSelected((*reinterpret_cast< QGraphicsItem*(*)>(_a[1]))); break;
+        case 20: _t->about(); break;
+        case 21: _t->saveTriggered(); break;
+        case 22: _t->saveAs(); break;
+        case 23: _t->copy(); break;
+        case 24: _t->cut(); break;
+        case 25: _t->paste(); break;
+        case 26: _t->undo(); break;
+        case 27: _t->redo(); break;
+        case 28: _t->group(); break;
+        case 29: _t->ungroup(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 8:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QGraphicsTextItem* >(); break;
+            }
+            break;
+        case 19:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QGraphicsItem* >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 30)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 30;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 30)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 30;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
